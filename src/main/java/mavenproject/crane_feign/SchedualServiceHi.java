450ef36f92742ad4fc534edc029a8fbde192cefa
qiugang <849888900@qq.com>
2018-09-19 11:26:45 +0800

    create

---
+++ src/main/java/mavenproject/crane_feign/SchedualServiceHi.java
@@ -0,0 +1,12 @@
+package mavenproject.crane_feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "eureka-client",fallback = SchedualServiceHiHystric.class)
+public interface SchedualServiceHi {
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name", defaultValue = "crane") String name);
+}
